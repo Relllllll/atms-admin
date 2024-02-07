be50bbd9ed0abe12f7df4e46c33b08617267f809
Kiritsugucarl <bluesniper017@gmail.com>
2024-02-07 14:06:43 +0800

    fix: searchBar

--- src/pages/Employee/Employee.jsx
+++ src/pages/Employee/Employee.jsx
@@ -33,26 +33,44 @@ const Employee = () => {
     return (
         <div className="main employee">
             <div className="employee__searchBox-wrapper">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="employee__searchBox-icon">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="employee__searchBox-icon"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+                    />
                 </svg>
-                <input className="employee__searchBox" type="text" placeholder="Search" name="searchBox" />
+                <input
+                    className="employee__searchBox"
+                    type="text"
+                    placeholder="Search"
+                    name="searchBox"
+                />
             </div>
-            <hr/>
-            <h1>Full Employee Details</h1>
-            <ul>
-                {Object.values(employees).map((employee) => (
-                    <li key={employee.key}>
-                        <p>
-                            Name: {employee.firstName} {employee.middleName}{" "}
-                            {employee.lastName}
-                        </p>
-                        <p>Contact Number: {employee.contactNum}</p>
-                        <p>Address: {employee.address}</p>
-                        <p>Age: {employee.age}</p>
-                    </li>
-                ))}
-            </ul>
+            <hr />
+            <div className="employee__list">
+                <h1>Full Employee Details</h1>
+                <ul>
+                    {Object.values(employees).map((employee) => (
+                        <li key={employee.key}>
+                            <p>
+                                Name: {employee.firstName} {employee.middleName}{" "}
+                                {employee.lastName}
+                            </p>
+                            <p>Contact Number: {employee.contactNum}</p>
+                            <p>Address: {employee.address}</p>
+                            <p>Age: {employee.age}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 };
