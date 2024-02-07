700365f8832e5088446f3ac2c2280e5617726b72
aciamdesu <shalomjamaicaave@gmail.com>
2024-02-07 13:49:25 +0800

    add: searchBar

--- src/pages/Employee/Employee.jsx
+++ src/pages/Employee/Employee.jsx
@@ -7,6 +7,7 @@ import "./Employee.css";
 
 const Employee = () => {
     const [employees, setEmployees] = useState({});
+    const [searchInput, setSearchInput] = useState();
     const [user] = useAuthState(auth);
     const navigate = useNavigate();
 
@@ -30,8 +31,15 @@ const Employee = () => {
         );
     }, []); // Empty dependency array to run only once
     return (
-        <div className="main">
-            <h1>Employee</h1>
+        <div className="main employee">
+            <div className="employee__searchBox-wrapper">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="employee__searchBox-icon">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                </svg>
+                <input className="employee__searchBox" type="text" placeholder="Search" name="searchBox" />
+            </div>
+            <hr/>
+            <h1>Full Employee Details</h1>
             <ul>
                 {Object.values(employees).map((employee) => (
                     <li key={employee.key}>
