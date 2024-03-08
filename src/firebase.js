3ee85121f6c11bdbe84ed5470e3b31203d1db65a
Relllllll <157577771+Relllllll@users.noreply.github.com>
2024-03-08 17:15:51 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,16 +8,27 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+//uscentral
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDwJYk1WnQk7GCJK_-xB8Bi0xlFPc1tVfY",
+//     authDomain: "attendance-management-sy-a4367.firebaseapp.com",
+//     databaseURL:
+//         "https://attendance-management-sy-a4367-default-rtdb.firebaseio.com",
+//     projectId: "attendance-management-sy-a4367",
+//     storageBucket: "attendance-management-sy-a4367.appspot.com",
+//     messagingSenderId: "1045091431522",
+//     appId: "1:1045091431522:web:3620ace02e02ee4db94d3b",
+//     measurementId: "G-428RXE7SQ9",
+// };
 const firebaseConfig = {
-    apiKey: "AIzaSyDwJYk1WnQk7GCJK_-xB8Bi0xlFPc1tVfY",
-    authDomain: "attendance-management-sy-a4367.firebaseapp.com",
-    databaseURL:
-        "https://attendance-management-sy-a4367-default-rtdb.firebaseio.com",
-    projectId: "attendance-management-sy-a4367",
-    storageBucket: "attendance-management-sy-a4367.appspot.com",
-    messagingSenderId: "1045091431522",
-    appId: "1:1045091431522:web:3620ace02e02ee4db94d3b",
-    measurementId: "G-428RXE7SQ9",
+  apiKey: "AIzaSyCj3fmuMRcOr0QHUsU5taLyLU2_wn5AHGQ",
+  authDomain: "atms-capstone.firebaseapp.com",
+  databaseURL: "https://atms-capstone-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "atms-capstone",
+  storageBucket: "atms-capstone.appspot.com",
+  messagingSenderId: "63113783163",
+  appId: "1:63113783163:web:1bd909f81ff09420f5ffde",
+  measurementId: "G-DM73FQC28K"
 };
 
 // Initialize Firebase
